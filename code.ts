df8ed1180749013654fa1a4629eb5eacbdda7f29
Brian Ujiie <>
2021-11-02 20:54:47 -0700

    Convert frame to PNG and set as frame image fill.

--- code.ts
+++ code.ts
@@ -1,34 +1,35 @@
-
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
-figma.showUI(__html__);
-
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createRectangle();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
-    }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  async function convertToPNG(node: FrameNode, paint) {
+      if (paint.type == "IMAGE") {
+            const bytes = await node.exportAsync()
+            figma.showUI(__html__, { visible: false })
+  
+            // Send the raw bytes of the file to the worker
+            figma.ui.postMessage(bytes)
+            const newBytes: Uint8Array = await new Promise((resolve, reject) => {
+                figma.ui.onmessage = value => resolve(value as Uint8Array)
+              })
+          
+              // Create a new paint for the new image. Uploading the image will give us
+              // an image hash.
+              const newPaint = JSON.parse(JSON.stringify(paint))
+              newPaint.imageHash = figma.createImage(newBytes).hash
+                return newPaint        
+      }
   }
-
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  figma.closePlugin();
-};
+ 
+  async function frameToPNG(node) {
+    const newFills = []
+    for (const paint of node.fills) {
+      newFills.push(await convertToPNG(node, paint))
+    }
+    node.fills = newFills
+    }
+  
+  
+  // This plugin looks at all the currently selected nodes and inverts the colors
+  // in their image, if they use an image paint.
+  Promise.all(figma.currentPage.selection.filter(node => node.type == 'FRAME').map(selected => {
+      console.log(selected)
+      frameToPNG(selected)
+    }))
+         .then(() => figma.closePlugin())
