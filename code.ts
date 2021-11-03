3800b2c257a3fadefa5b945e97c6d5cebdc3c2b6
Brian Ujiie <>
2021-11-02 22:48:17 -0700

    Fixed issue with .map() lambda not returning anything.

--- code.ts
+++ code.ts
@@ -1,35 +1,32 @@
-  async function convertToPNG(node: FrameNode, paint) {
-      if (paint.type == "IMAGE") {
-            const bytes = await node.exportAsync()
-            figma.showUI(__html__, { visible: false })
-  
-            // Send the raw bytes of the file to the worker
-            figma.ui.postMessage(bytes)
-            const newBytes: Uint8Array = await new Promise((resolve, reject) => {
-                figma.ui.onmessage = value => resolve(value as Uint8Array)
-              })
-          
-              // Create a new paint for the new image. Uploading the image will give us
-              // an image hash.
-              const newPaint = JSON.parse(JSON.stringify(paint))
-              newPaint.imageHash = figma.createImage(newBytes).hash
-                return newPaint        
-      }
-  }
- 
-  async function frameToPNG(node) {
+async function convertToPNG(node: FrameNode, paint) {
+    if (paint.type == "IMAGE") {
+        const bytes = await node.exportAsync()
+        figma.showUI(__html__, { visible: false })
+
+        // Send the raw bytes of the file to the worker
+        figma.ui.postMessage(bytes)
+        const newBytes: Uint8Array = await new Promise((resolve, reject) => {
+            figma.ui.onmessage = value => resolve(value as Uint8Array)
+        })
+
+        // Create a new paint for the new image. Uploading the image will give us
+        // an image hash.
+        const newPaint = JSON.parse(JSON.stringify(paint))
+        newPaint.imageHash = figma.createImage(newBytes).hash
+        return newPaint
+    }
+}
+
+async function frameToPNG(node) {
     const newFills = []
     for (const paint of node.fills) {
-      newFills.push(await convertToPNG(node, paint))
+        newFills.push(await convertToPNG(node, paint))
     }
     node.fills = newFills
-    }
-  
-  
-  // This plugin looks at all the currently selected nodes and inverts the colors
-  // in their image, if they use an image paint.
-  Promise.all(figma.currentPage.selection.filter(node => node.type == 'FRAME').map(selected => {
-      console.log(selected)
-      frameToPNG(selected)
-    }))
-         .then(() => figma.closePlugin())
+}
+
+Promise
+    .all(figma.currentPage.selection
+        .filter(node => node.type == 'FRAME')
+        .map(selected => frameToPNG(selected)))
+    .then(() => figma.closePlugin())
