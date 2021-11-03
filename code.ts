728de3920236ebb4cea10ad1a20bc651794cce77
Brian Ujiie <>
2021-11-02 22:54:39 -0700

    Add the converged PNG to a new image fill.

--- code.ts
+++ code.ts
@@ -1,27 +1,26 @@
-async function convertToPNG(node: FrameNode, paint) {
-    if (paint.type == "IMAGE") {
-        const bytes = await node.exportAsync()
-        figma.showUI(__html__, { visible: false })
+async function convertToPNG(node: FrameNode): Promise<ImagePaint> {
+    const bytes = await node.exportAsync()
+    figma.showUI(__html__, { visible: false })
 
-        // Send the raw bytes of the file to the worker
-        figma.ui.postMessage(bytes)
-        const newBytes: Uint8Array = await new Promise((resolve, reject) => {
-            figma.ui.onmessage = value => resolve(value as Uint8Array)
-        })
+    // Send the raw bytes of the file to the worker
+    figma.ui.postMessage(bytes)
+    const newBytes: Uint8Array = await new Promise((resolve, reject) => {
+        figma.ui.onmessage = value => resolve(value as Uint8Array)
+    })
 
-        // Create a new paint for the new image. Uploading the image will give us
-        // an image hash.
-        const newPaint = JSON.parse(JSON.stringify(paint))
-        newPaint.imageHash = figma.createImage(newBytes).hash
-        return newPaint
+    return {
+        type: "IMAGE",
+        scaleMode: "FILL",
+        imageHash: figma.createImage(newBytes).hash
     }
 }
 
 async function frameToPNG(node) {
-    const newFills = []
+    const newFills: Array<Paint> = []
     for (const paint of node.fills) {
-        newFills.push(await convertToPNG(node, paint))
+        newFills.push(paint)
     }
+    newFills.push(await convertToPNG(node))
     node.fills = newFills
 }
 
